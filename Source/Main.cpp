d068bd26c6889584610edcc4c442079b93c8dbe1
fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2022-11-11 00:07:50 -0500

    Added .clang-format and formatted Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,118 +1,117 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-#include <vector>
-#include <stdexcept>
 #include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
 
-const uint32_t WIDTH = 800;
+const uint32_t WIDTH  = 800;
 const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication
 {
-public:
-	void Run ( )
-	{
-		InitWindow ( );
-		CreateInstance ( );
-		InitVulkan ( );
-		MainLoop ( );
-		Cleanup ( );
-	}
+ public:
+  void Run ( )
+  {
+    InitWindow ( );
+    CreateInstance ( );
+    InitVulkan ( );
+    MainLoop ( );
+    Cleanup ( );
+  }
 
-private:
-	GLFWwindow* Window;
-	VkInstance Instance;
+ private:
+  GLFWwindow* Window;
+  VkInstance  Instance;
 
-	void InitWindow ( )
-	{
-		glfwInit ( );
+  void InitWindow ( )
+  {
+    glfwInit ( );
 
-		glfwWindowHint ( GLFW_CLIENT_API, GLFW_NO_API );
-		glfwWindowHint ( GLFW_RESIZABLE, GLFW_FALSE );
+    glfwWindowHint ( GLFW_CLIENT_API, GLFW_NO_API );
+    glfwWindowHint ( GLFW_RESIZABLE, GLFW_FALSE );
 
-		Window = glfwCreateWindow ( WIDTH, HEIGHT, "Vulkan", nullptr, nullptr );
-	}
+    Window = glfwCreateWindow ( WIDTH, HEIGHT, "Vulkan", nullptr, nullptr );
+  }
 
-	void CreateInstance ( )
-	{
-		VkApplicationInfo AppInfo {};
-		AppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-		AppInfo.pApplicationName = "Hello Triangle";
-		AppInfo.applicationVersion = VK_MAKE_VERSION ( 1, 0, 0 );
-		AppInfo.pEngineName = "No Engine";
-		AppInfo.engineVersion = VK_MAKE_VERSION ( 1, 0, 0 );
-		AppInfo.apiVersion = VK_API_VERSION_1_0;
+  void CreateInstance ( )
+  {
+    VkApplicationInfo AppInfo { };
+    AppInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    AppInfo.pApplicationName   = "Hello Triangle";
+    AppInfo.applicationVersion = VK_MAKE_VERSION ( 1, 0, 0 );
+    AppInfo.pEngineName        = "No Engine";
+    AppInfo.engineVersion      = VK_MAKE_VERSION ( 1, 0, 0 );
+    AppInfo.apiVersion         = VK_API_VERSION_1_0;
 
-		VkInstanceCreateInfo CreateInfo {};
-		CreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-		CreateInfo.pApplicationInfo = &AppInfo;
+    VkInstanceCreateInfo CreateInfo { };
+    CreateInfo.sType            = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    CreateInfo.pApplicationInfo = &AppInfo;
 
-		uint32_t GLFWExtensionCount = 0;
-		const char** GLFWExtensions = nullptr;
+    uint32_t     GLFWExtensionCount = 0;
+    const char** GLFWExtensions     = nullptr;
 
-		vkEnumerateInstanceExtensionProperties ( nullptr, &GLFWExtensionCount, nullptr );
-		std::vector<VkExtensionProperties> Extensions ( GLFWExtensionCount );
-		vkEnumerateInstanceExtensionProperties ( nullptr, &GLFWExtensionCount, Extensions.data ( ) );
+    vkEnumerateInstanceExtensionProperties (
+        nullptr,
+        &GLFWExtensionCount,
+        nullptr );
+    std::vector< VkExtensionProperties > Extensions ( GLFWExtensionCount );
+    vkEnumerateInstanceExtensionProperties (
+        nullptr,
+        &GLFWExtensionCount,
+        Extensions.data ( ) );
 
-		std::cout << "available extensions (" << GLFWExtensionCount << "):\n";
+    std::cout << "available extensions (" << GLFWExtensionCount << "):\n";
 
-		for ( const auto& Extension : Extensions )
-		{
-			std::cout << '\t' << Extension.extensionName << '\n';
-		}
+    for ( const auto& Extension : Extensions )
+      {
+        std::cout << '\t' << Extension.extensionName << '\n';
+      }
 
-		GLFWExtensions = glfwGetRequiredInstanceExtensions ( &GLFWExtensionCount );
+    GLFWExtensions = glfwGetRequiredInstanceExtensions ( &GLFWExtensionCount );
 
-		CreateInfo.enabledExtensionCount = GLFWExtensionCount;
-		CreateInfo.ppEnabledExtensionNames = GLFWExtensions;
+    CreateInfo.enabledExtensionCount   = GLFWExtensionCount;
+    CreateInfo.ppEnabledExtensionNames = GLFWExtensions;
 
-		CreateInfo.enabledLayerCount = 0;
+    CreateInfo.enabledLayerCount = 0;
 
-		if ( vkCreateInstance ( &CreateInfo, nullptr, &Instance ) != VK_SUCCESS )
-		{
-			throw std::runtime_error ( "failed to create instance!" );
-		}
+    if ( vkCreateInstance ( &CreateInfo, nullptr, &Instance ) != VK_SUCCESS )
+      {
+        throw std::runtime_error ( "failed to create instance!" );
+      }
+  }
 
-	}
+  void InitVulkan ( ) {}
 
-	void InitVulkan ( )
-	{
+  void MainLoop ( )
+  {
+    while ( !glfwWindowShouldClose ( Window ) ) { glfwPollEvents ( ); }
+  }
 
-	}
+  void Cleanup ( )
+  {
+    vkDestroyInstance ( Instance, nullptr );
 
-	void MainLoop ( )
-	{
-		while ( !glfwWindowShouldClose ( Window ) )
-		{
-			glfwPollEvents ( );
-		}
-	}
+    glfwDestroyWindow ( Window );
 
-	void Cleanup ( )
-	{
-		vkDestroyInstance ( Instance, nullptr );
-
-		glfwDestroyWindow ( Window );
-
-		glfwTerminate ( );
-
-	}
+    glfwTerminate ( );
+  }
 };
 
 int main ( )
 {
-	HelloTriangleApplication App{};
+  HelloTriangleApplication App { };
 
-	try
-	{
-		App.Run ( );
-	} catch ( const std::exception& e )
-	{
-		std::cerr << e.what ( ) << std::endl;
-		return EXIT_FAILURE;
-	}
+  try
+    {
+      App.Run ( );
+    }
+  catch ( const std::exception& e )
+    {
+      std::cerr << e.what ( ) << std::endl;
+      return EXIT_FAILURE;
+    }
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
